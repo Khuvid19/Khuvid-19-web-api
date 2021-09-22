08cd390ec5371caaf9131aaa214e28d7c9d272eb
Jung Ju Hee <jieboong82@daum.net>
2021-09-22 16:20:16 +0900

    project init

---
+++ src/main/java/khuvid19/vaccinated/VaccinatedApplication.java
@@ -0,0 +1,13 @@
+package khuvid19.vaccinated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinatedApplication.class, args);
+	}
+
+}
