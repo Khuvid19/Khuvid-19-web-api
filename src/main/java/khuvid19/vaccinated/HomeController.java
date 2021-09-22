e89cff1d5a97f0dc2145e5d9f94c239fe9aeed28
Jung Ju Hee <jieboong82@daum.net>
2021-09-22 16:20:16 +0900

    project init

---
+++ src/main/java/khuvid19/vaccinated/HomeController.java
@@ -0,0 +1,13 @@
+package khuvid19.vaccinated;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String Home(){
+        return "home";
+    }
+}
