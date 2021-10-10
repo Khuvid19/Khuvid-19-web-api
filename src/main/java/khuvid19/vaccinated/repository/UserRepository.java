d3364acb5ab86d3c05e8e02e8142c70564a4752f
Jung Ju Hee <jieboong82@khu.ac.kr>
2021-10-11 01:42:06 +0900

    Data struct 추가

---
+++ src/main/java/khuvid19/vaccinated/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package khuvid19.vaccinated.repository;
+
+import khuvid19.vaccinated.dao.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
