a11572f475fdec7984b9c032daf92e656b61cc58
Jung Ju Hee <jieboong82@khu.ac.kr>
2021-10-17 15:37:16 +0900

    email대신 token사용으로 코드 변경

--- src/main/java/khuvid19/vaccinated/repository/UserRepository.java
+++ src/main/java/khuvid19/vaccinated/repository/UserRepository.java
@@ -4,9 +4,14 @@ import khuvid19.vaccinated.dao.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByUserName(String userName);
+    Optional<User> findByAccessToken(String accessToken);
+    boolean existsByUserName(String userName);
 }
