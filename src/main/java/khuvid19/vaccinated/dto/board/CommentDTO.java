0e94a315eb4b6d01ee326f026fa9c38bb401779b
Jung Ju Hee <jieboong82@khu.ac.kr>
2021-10-18 13:48:29 +0900

    댓글 작성 기능 구현

--- src/main/java/khuvid19/vaccinated/dto/board/CommentDTO.java
+++ src/main/java/khuvid19/vaccinated/dto/board/CommentDTO.java
@@ -1,2 +1,13 @@
-package khuvid19.vaccinated.dto.board;public class CommentDTO {
+package khuvid19.vaccinated.dto.board;
+
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+public class CommentDTO {
+    String content;
+    String accessToken;
+    Date date;
+    Long boardId;
 }
