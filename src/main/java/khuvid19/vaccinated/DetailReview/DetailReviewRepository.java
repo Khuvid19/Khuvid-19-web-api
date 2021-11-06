e46615371c0c0a51aefebfcc6a77ed08659e81ea
Kang_Taewoo <taewookang96@gmail.com>
2021-11-07 04:50:21 +0900

    SimpleReviewService 구현 및 상세 후기 작성 API 구현

---
+++ src/main/java/khuvid19/vaccinated/DetailReview/DetailReviewRepository.java
@@ -0,0 +1,8 @@
+package khuvid19.vaccinated.DetailReview;
+
+import khuvid19.vaccinated.DetailReview.Data.DetailReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetailReviewRepository extends JpaRepository<DetailReview, Long> {
+
+}
