d3c9ef2a02b48a5dc41cc0ed6bb179a8f455d8c7
Juhee <jieboong82@github.com>
2021-11-07 19:20:20 +0900

    Spring Security dependency 제거

--- src/main/java/khuvid19/vaccinated/service/UserService.java
+++ src/main/java/khuvid19/vaccinated/service/UserService.java
@@ -1,11 +1,9 @@
 package khuvid19.vaccinated.service;
 
-import khuvid19.vaccinated.JwtTokenProvider;
 import khuvid19.vaccinated.dao.User;
 import khuvid19.vaccinated.dto.login.GoogleUser;
 import khuvid19.vaccinated.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -16,12 +14,10 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository userRepository;
     private final OAuthService oAuthService;
-    private final JwtTokenProvider jwtTokenProvider;
 
-    public UserService(UserRepository userRepository, OAuthService oAuthService, JwtTokenProvider jwtTokenProvider) {
+    public UserService(UserRepository userRepository, OAuthService oAuthService) {
         this.userRepository = userRepository;
         this.oAuthService = oAuthService;
-        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     public User oauthLogin(String access_token){
