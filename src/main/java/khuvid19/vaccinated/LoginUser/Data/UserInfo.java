3372dcb5f53c8ff6d7e614f07b9f6b6db98baff5
Juhee <jieboong82@github.com>
2021-11-12 23:24:43 +0900

    jwt 이요한 로그인 및 유저 정보 입력 설정

--- src/main/java/khuvid19/vaccinated/LoginUser/Data/UserInfo.java
+++ src/main/java/khuvid19/vaccinated/LoginUser/Data/UserInfo.java
@@ -5,13 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class UserInfo {
     String google_name;
-    String userName;
+    String nickName;
     String email;
     String picUrl;
-
+    Date birthday;
+    String gender;
 }
