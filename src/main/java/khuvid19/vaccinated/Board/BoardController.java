9114418b5ebcee25f1c2cc15fac749254a58a84c
Juhee <jieboong82@github.com>
2021-11-12 23:43:34 +0900

    jwt 인증으로 자유게시판 작성 로직 변경

--- src/main/java/khuvid19/vaccinated/Board/BoardController.java
+++ src/main/java/khuvid19/vaccinated/Board/BoardController.java
@@ -2,11 +2,16 @@ package khuvid19.vaccinated.Board;
 
 import khuvid19.vaccinated.Board.Data.Board;
 import khuvid19.vaccinated.Board.Data.BoardInfo;
+import khuvid19.vaccinated.Board.Data.Comment;
 import khuvid19.vaccinated.Board.Data.CommentInfo;
+import khuvid19.vaccinated.LoginUser.Data.SecurityUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Security;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -16,13 +21,13 @@ public class BoardController {
     private final BoardService boardService;
 
     @PostMapping("/board")
-    public HttpStatus newBoard(@RequestBody BoardInfo postBoard) {
-        return boardService.saveBoard(postBoard);
+    public HttpStatus newBoard(@RequestBody BoardInfo postBoard, @AuthenticationPrincipal SecurityUser securityUser) {
+        return boardService.saveBoard(securityUser.getUser(),postBoard);
     }
 
     @PostMapping("/board/comment")
-    public HttpStatus newComment(@RequestBody CommentInfo comment) {
-        return boardService.newComment(comment);
+    public HttpStatus newComment(@RequestBody CommentInfo commentInfo, @AuthenticationPrincipal SecurityUser securityUser) {
+        return boardService.newComment(securityUser.getUser(), commentInfo);
     }
 
     @GetMapping("/board/list")
