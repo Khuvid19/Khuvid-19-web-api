722f91cfb81d2bb316d947178671c4051a96e075
Kang_Taewoo <taewookang96@gmail.com>
2021-11-14 02:22:32 +0900

    리뷰 중복 추가 방지 로직 구현

--- src/main/java/khuvid19/vaccinated/Review/Data/ReviewRepository.java
+++ src/main/java/khuvid19/vaccinated/Review/Data/ReviewRepository.java
@@ -1,10 +1,12 @@
 package khuvid19.vaccinated.Review.Data;
 
+import khuvid19.vaccinated.Constants.VaccineType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, String> {
     List<Review> findAll();
+    Boolean existsReviewByUserIdAndVaccine(Long userId, VaccineType vaccine);
 }
 
--- src/main/java/khuvid19/vaccinated/Review/ReviewService.java
+++ src/main/java/khuvid19/vaccinated/Review/ReviewService.java
@@ -2,10 +2,11 @@ package khuvid19.vaccinated.Review;
 
 import khuvid19.vaccinated.Constants.SideEffectType;
 import khuvid19.vaccinated.Constants.VaccineType;
-import khuvid19.vaccinated.SideEffects.SideEffectsService;
 import khuvid19.vaccinated.Review.Data.Review;
 import khuvid19.vaccinated.Review.Data.ReviewRepository;
+import khuvid19.vaccinated.SideEffects.SideEffectsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,10 +22,20 @@ public class ReviewService {
         return reviewRepository.findAll();
     }
 
-    public void insertSimpleReview(Review receivedReview) {
+    public HttpStatus insertSimpleReview(Review receivedReview) {
         List<SideEffectType> inputSideEffectTypes = receivedReview.getSideEffects();
         VaccineType inputVaccineType = receivedReview.getVaccine();
+
+        Boolean isDuplicatedReview = reviewRepository.existsReviewByUserIdAndVaccine(
+                receivedReview.getUserId(), receivedReview.getVaccine()
+        );
+
+        if (isDuplicatedReview) {
+            return HttpStatus.GONE;
+        }
+
         reviewRepository.save(receivedReview);
         sideEffectsService.addSideEffectsCount(inputSideEffectTypes, inputVaccineType);
+        return HttpStatus.OK;
     }
 }
