8d7794857be91593265505dee2a13a08a12fb375
Kang_Taewoo <taewookang96@gmail.com>
2021-11-14 02:30:37 +0900

    백신 한국어 버젼 데이터 추가

--- src/main/java/khuvid19/vaccinated/Constants/VaccineType.java
+++ src/main/java/khuvid19/vaccinated/Constants/VaccineType.java
@@ -1,6 +1,19 @@
 package khuvid19.vaccinated.Constants;
 
 public enum VaccineType {
-    Ansen, Pfizer, Moderna, Astrazeneka
+    ANSEN("얀센"), ANSEN_BOOST("얀센 부스터샷"),
+    PFIZER_FIRST("화이자 1차"), PFIZER_SECOND("화이자 2차"),
+    MODERNA_FIRST("모더나 1차"), MODERNA_SECOND("모더나 2차"),
+    ASTRAZENEKA_FIRST("아스트라제네카 1차"), ASTRAZENEKA_SECOND("아스트라제네카 1차");
 
+    private final String koreanName;
+
+    VaccineType(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
+    public String getKoreanName() {
+        return koreanName;
+    }
 }
+
