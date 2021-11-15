12419a6972c69889b50ace46618cf61ae1198555
Juhee <jieboong82@github.com>
2021-11-16 00:53:10 +0900

    Enum api 구현

--- src/main/java/khuvid19/vaccinated/Constants/Gender.java
+++ src/main/java/khuvid19/vaccinated/Constants/Gender.java
@@ -1,5 +1,8 @@
 package khuvid19.vaccinated.Constants;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Gender {
     FEMALE("여성"), MALE("남성");
 
@@ -7,5 +10,15 @@ public enum Gender {
 
     Gender(String koreanName) {
         this.koreanName = koreanName;
+    }
+
+    public static Map<Gender, String> getAllTypes(){
+        Map<Gender, String> result;
+        result = new HashMap<>();
+        for (Gender type :
+                Gender.values()) {
+            result.put(type, type.koreanName);
+        }
+        return result;
     }
 }
