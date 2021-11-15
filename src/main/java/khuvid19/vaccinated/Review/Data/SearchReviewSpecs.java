e76b86da98d0f563d8b7e0fb8ef3378dc0db849c
Kang_Taewoo <taewookang96@gmail.com>
2021-11-16 03:22:53 +0900

    성별, 나이 필터 검색 기능 추가

--- src/main/java/khuvid19/vaccinated/Review/Data/SearchReviewSpecs.java
+++ src/main/java/khuvid19/vaccinated/Review/Data/SearchReviewSpecs.java
@@ -1,5 +1,7 @@
 package khuvid19.vaccinated.Review.Data;
 
+import khuvid19.vaccinated.Constants.AgeType;
+import khuvid19.vaccinated.Constants.Gender;
 import khuvid19.vaccinated.Constants.SideEffectType;
 import khuvid19.vaccinated.Constants.VaccineType;
 import org.springframework.data.jpa.domain.Specification;
@@ -19,6 +21,14 @@ public class SearchReviewSpecs {
 
     public static Specification<Review> sideEffectContains (List<SideEffectType> types) {
         return (root, query, criteriaBuilder) -> criteriaBuilder.in(root.get("sideEffects").in(types));
+    }
+
+    public static Specification<Review> genderEqual (Gender gender) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.in(root.get("user").get("gender").in(gender));
+    }
+
+    public static Specification<Review> ageEqual (AgeType age) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.in(root.get("user").get("age").in(age));
     }
 
     public static Specification<Review> inoculatedBetween (Date start, Date end) {
