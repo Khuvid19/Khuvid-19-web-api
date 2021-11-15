e2342781813f05ca4ac253f65009fba5c87a5837
Juhee <jieboong82@github.com>
2021-11-16 00:53:10 +0900

    Enum api 구현

--- src/main/java/khuvid19/vaccinated/LoginUser/UserController.java
+++ src/main/java/khuvid19/vaccinated/LoginUser/UserController.java
@@ -1,6 +1,8 @@
 package khuvid19.vaccinated.LoginUser;
 
 import khuvid19.vaccinated.Configuration.JwtTokenProvider;
+import khuvid19.vaccinated.Constants.AgeType;
+import khuvid19.vaccinated.Constants.Gender;
 import khuvid19.vaccinated.LoginUser.Data.PostUser;
 import khuvid19.vaccinated.LoginUser.Data.SecurityUser;
 import khuvid19.vaccinated.LoginUser.Data.User;
@@ -12,31 +14,44 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequiredArgsConstructor
+@RequestMapping("auth")
 @Slf4j
 public class UserController {
 
     private final UserService userService;
     private final UserRepository userRepository;
 
-    @PostMapping("/auth/user")
+    @PostMapping("/user")
     public UserInfo setUserInfo(@AuthenticationPrincipal SecurityUser user, PostUser postUser) {
         return userService.setUserInfo(user.getUser(), postUser);
     }
 
-    @GetMapping("/auth/user")
+    @GetMapping("/user")
     public boolean isNickNameExists(@RequestParam String nickName) {
         return userRepository.existsByNickName(nickName);
     }
 
-    @PostMapping("/auth/google")
+    @PostMapping("/google")
     public User loginByToken(@RequestParam(value = "access_token") String access_token) {
         User user = userService.oauthLogin(access_token);
         return user;
     }
 
+    @GetMapping("/types/age")
+    public Map<AgeType, String> getAgeTypes() {
+        return AgeType.getAllTypes();
+    }
 
+    @GetMapping("/types/gender")
+    public Map<Gender, String> getGenderType(){
+        return Gender.getAllTypes();
+    }
 }
