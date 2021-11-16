8043d33aa46dcdaeaa190360ace0793cd4f276f7
Juhee <jieboong82@github.com>
2021-11-16 15:35:36 +0900

    Covid Data Api 및 스케쥴러 구현

--- src/main/java/khuvid19/vaccinated/Configuration/SecurityConfig.java
+++ src/main/java/khuvid19/vaccinated/Configuration/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "/swagger-resources/**",
                 "/configuration/security",
                 "/swagger-ui.html",
+                "/h2-console/**",
                 "/webjars/**");
     }
 
--- src/main/java/khuvid19/vaccinated/SituationBoard/SituationController.java
+++ src/main/java/khuvid19/vaccinated/SituationBoard/SituationController.java
@@ -1,56 +1,99 @@
 package khuvid19.vaccinated.SituationBoard;
 
 import khuvid19.vaccinated.SituationBoard.Data.CovidData;
-import khuvid19.vaccinated.SituationBoard.Data.CovidInfo;
 import khuvid19.vaccinated.SituationBoard.Data.CovidResponse;
+import khuvid19.vaccinated.SituationBoard.Data.CovidResponseData;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.text.SimpleDateFormat;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.time.LocalDate;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class SituationController {
 
-    @Value("${portal.secretkey}") private String portalKey;
-    @Value("${portal.url}") private String portalUrl;
+    @Value("${portal.secretkey}")
+    private String portalKey;
+    @Value("${portal.url}")
+    private String portalUrl;
 
-    private final CovidInfoRepository infoRepo;
+    private final CovidRepository covidRepository;
 
-    @GetMapping("/covid")
-    public CovidInfo getCovidInfo() {
-        return infoRepo.findByDate(LocalDate.now()).get();
-    }
+//    @GetMapping("/covid")
+//    public CovidResponse getCovidInfo() {
+//        return infoRepo.findByDate(LocalDate.now()).get();
+//    }
 
     @GetMapping("/test")
-    public CovidInfo get(){
+    public Object get() throws UnsupportedEncodingException {
 
-        Date today = new Date();
-        SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
-        params.add("ServiceKey", portalKey);
-        params.add("startCreateDt",df.format(today));
+        String serviceKey_Decoder = URLDecoder.decode(portalKey.toString(), "UTF-8");
 
-        String url = portalUrl + "?ServiceKey=" + portalKey + "&startCreateDt=" + df.format(today);
+        String url = portalUrl + "?ServiceKey=" + serviceKey_Decoder + "&startCreateDt=" + today.format(formatter);
 
         RestTemplate restTemplate = new RestTemplate();
         CovidResponse response = restTemplate.getForObject(url, CovidResponse.class);
-        log.info(response.getItems().getItems().toString());
 
-        CovidData covidData = response.getItems().getItems().get(0);
+        CovidResponseData covidResponseData = response.getBody().getItems().get(0);
+        Optional<CovidData> yesterday = covidRepository.findByDate(LocalDate.now().minusDays(1));
 
-        CovidInfo info = new CovidInfo(covidData.getCreateDt().toLocalDate(), covidData.getDecideCnt());
+        CovidData todayData;
+        if (yesterday.isPresent()) {
+            todayData = new CovidData(LocalDate.parse(covidResponseData.getStateDt(), formatter),
+                    covidResponseData.getDecideCnt(), covidResponseData.getDecideCnt() - yesterday.get().getDecideCnt());
+        } else {
+            todayData = new CovidData(LocalDate.parse(covidResponseData.getStateDt(), formatter), covidResponseData.getDecideCnt());
 
-        return infoRepo.save(info);
+        }
+        return covidRepository.save(todayData);
+    }
+
+    @GetMapping("/yesterday")
+    public Object yester() throws UnsupportedEncodingException {
+
+        LocalDate today = LocalDate.now().minusDays(1L);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        String serviceKey_Decoder = URLDecoder.decode(portalKey.toString(), "UTF-8");
+
+        String url = portalUrl + "?ServiceKey=" + serviceKey_Decoder + "&startCreateDt=" + today.format(formatter) + "&endCreateDt=" + today.format(formatter);
+
+        log.info(url);
+
+        RestTemplate restTemplate = new RestTemplate();
+        CovidResponse response = restTemplate.getForObject(url, CovidResponse.class);
+
+        CovidResponseData covidResponseData = response.getBody().getItems().get(0);
+
+        CovidData todayData = new CovidData(LocalDate.parse(covidResponseData.getStateDt(), formatter), covidResponseData.getDecideCnt());
+
+        return covidRepository.save(todayData);
+
+    }
+
+    @GetMapping("/covid")
+    public CovidData getCovidData(){
+        LocalDate today = LocalDate.now();
+
+        Optional<CovidData> todayDate = covidRepository.findByDate(today);
+
+        if (todayDate.isPresent()) {
+            return todayDate.get();
+        }
+        else return null;
     }
 }
