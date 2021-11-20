0059ed9aea2c9c1708fa6edfe33b8271f567d447
Juhee <jieboong82@github.com>
2021-11-20 19:08:17 +0900

    Tomcat Timezone 설정

--- src/main/java/khuvid19/vaccinated/VaccinatedApplication.java
+++ src/main/java/khuvid19/vaccinated/VaccinatedApplication.java
@@ -3,8 +3,17 @@ package khuvid19.vaccinated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class VaccinatedApplication {
+
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VaccinatedApplication.class, args);
