dac86de05927ccc6643ba8562fdeb2d958ae9ae3
Kang_Taewoo <taewookang96@gmail.com>
2021-11-25 01:16:29 +0900

    아스트라제네카 이름 변경

--- src/main/java/khuvid19/vaccinated/Constants/VaccineType.java
+++ src/main/java/khuvid19/vaccinated/Constants/VaccineType.java
@@ -7,7 +7,7 @@ public enum VaccineType {
     ANSEN("얀센"), ANSEN_BOOST("얀센 부스터샷"),
     PFIZER_FIRST("화이자 1차"), PFIZER_SECOND("화이자 2차"),
     MODERNA_FIRST("모더나 1차"), MODERNA_SECOND("모더나 2차"),
-    ASTRAZENEKA_FIRST("아스트라제네카 1차"), ASTRAZENEKA_SECOND("아스트라제네카 2차");
+    AZ_FIRST("AZ 1차"), AZ_SECOND("AZ 2차");
 
     private final String koreanName;
 
