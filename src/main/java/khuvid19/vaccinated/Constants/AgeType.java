bdeafb9fa3295a30be0af91c5ac797e6eefbf647
Juhee <jieboong82@github.com>
2021-12-01 22:36:23 +0900

    10대 이하 타입 제거

--- src/main/java/khuvid19/vaccinated/Constants/AgeType.java
+++ src/main/java/khuvid19/vaccinated/Constants/AgeType.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public enum AgeType {
-    UNDER_TEEN("10대 이하"),TEENAGER("10대"), TWENTIES("20대"),
+    TEENAGER("10대"), TWENTIES("20대"),
     THIRTIES("30대"), FORTIES("40대"), FIFTIES("50대"),
     SIXTIES("60대"), OVER_SEVENTY("70대 이상");
 
